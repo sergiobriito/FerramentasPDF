243187851f0ed1189c83bb079e20cb574845333e
sergiobriito <64617586+sergiobriito@users.noreply.github.com>
2022-10-21 11:30:11 -0300

    Update App.py

--- App.py
+++ App.py
@@ -74,7 +74,7 @@ def ComprimirPDF(arquivoComprimir):
 
       entrada = "teste.pdf"
       saida = "Arquivo_Compress.pdf"
-      subprocess.Popen(["+x","./pdfsizeopt/pdfsizeopt.exe",entrada,saida], stdout=pdf, shell=True)
+      subprocess.Popen(["+x","./pdfsizeopt/pdfsizeopt.exe",entrada,saida], shell=True)
    
       #with open(saida,"rb") as arquivoFinal:
          #st.download_button(label ="📥 Download",data = arquivoFinal,file_name=saida)
