ffa03ab26ac05fe4765db9d3e8bba73b218cbebc
sergiobriito <64617586+sergiobriito@users.noreply.github.com>
2022-10-27 10:51:55 -0300

    Update App.py

--- App.py
+++ App.py
@@ -79,7 +79,7 @@ def ComprimirPDF(arquivoComprimir):
    os.system("tar -xzvf pdfsizeopt_libexec_linux.tar.gz")
    os.system("chmod +x pdfsizeopt.single")
    os.system("ln -sf pdfsizeopt.single pdfsizeopt")
-   os.system("sh  {} {} {}".format(compress,entrada,saida))
+   os.system("{} {} {}".format(compress,entrada,saida))
    
    #with open(saida,"rb") as arquivoFinal:
    #   st.download_button(label ="📥 Download",data = arquivoFinal,file_name=saida)
