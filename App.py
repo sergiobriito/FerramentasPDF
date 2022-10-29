353bb1b14f4e0d6c6545a73f742f2e42ae7cdc0b
sergiobriito <64617586+sergiobriito@users.noreply.github.com>
2022-10-29 14:23:57 -0300

    Update App.py

--- App.py
+++ App.py
@@ -297,8 +297,6 @@ section.css-po3vlj.exg6vvm15 button{visibility:hidden;}
 <a id="nome" class="badge-base__link LI-simple-link" href="https://br.linkedin.com/in/s%C3%A9rgio--brito?trk=profile-badge">Sérgio Brito</a>
 </div>
 
-<div id="googleVerification">google-site-verification: googlea69a5919d8fe5d0a.html</div>
-
 """
 
 st.markdown(style, unsafe_allow_html=True)
