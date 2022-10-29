00c005901d2f9d2f0201ea6f4525986fa0afc19a
sergiobriito <64617586+sergiobriito@users.noreply.github.com>
2022-10-29 11:15:13 -0300

    Update App.py

--- App.py
+++ App.py
@@ -244,7 +244,7 @@ if funcionalidaEscolhida == "Converter PDF para Word":
 
 style = """
 <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>  
-<script>
+<script async defer type="text/javascript">
 let element = document.querySelector("#root > div:nth-child(1) > div > div > a");
 element.hidden = true;
 </script> 
