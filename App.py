8fdd1e983e2e0f593aca2f4f1385329278633a6d
sergiobriito <64617586+sergiobriito@users.noreply.github.com>
2022-10-29 11:56:09 -0300

    Update App.py

--- App.py
+++ App.py
@@ -244,8 +244,8 @@ if funcionalidaEscolhida == "Converter PDF para Word":
          
 
 style = """
-<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-<script>
+
+<script async defer type="text/javascript">
 let element = document.querySelector("#root > div:nth-child(1) > div.withScreencast > div > div > div > section > div > div:nth-child(1) > div > div:nth-child(3) > div > section > button");
 element.innerText = "Procurar arquivos";
 </script>
